07ed8c32e4f3a54c1529413ad15eafdb377fb28a
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-11-15 16:40:39 -0800

    registering user added

--- Smack(10)/Controller/CreateAccountVC.swift
+++ Smack(10)/Controller/CreateAccountVC.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //Outlets
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else {return}
+        guard let pass = passTxt.text, passTxt.text != "" else {return}
+        
+        AuthService.instance.registerUsert(email: email, password: pass) { (success) in
+            if success{
+                print ("registred user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
 
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
+    
     
 }
