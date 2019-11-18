53cfc71701f39b2fd97de557e9c16e7a719a2fbe
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-11-18 14:09:24 -0800

    loggedIn interface 90

--- Smack(10)/Controller/ChannelVC.swift
+++ Smack(10)/Controller/ChannelVC.swift
@@ -13,6 +13,9 @@ class ChannelVC: UIViewController {
     //Outlets
     
     @IBOutlet weak var loginBtn: UIButton!
+    
+    @IBOutlet weak var userImg: CircleImage!
+    
     @IBAction func prepareForUnwind (seque: UIStoryboardSegue){
         
     }
@@ -21,11 +24,24 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
         
       self.revealViewController().rearViewRevealWidth = self.view.frame.size.width-60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_notif:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
 
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataDidChange(_notif:Notification){
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named:"menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
     }
     
 
